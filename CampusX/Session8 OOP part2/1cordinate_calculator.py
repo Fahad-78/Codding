8094271ce46bb5b94602872ee54bce156932517f
Fahad-78 <fahadsorkar23@gmail.com>
2026-04-11 16:38:49 +0600

    Add line class with methods for point-line relationship and shortest distance calculation

--- CampusX/Session8 OOP part2/1cordinate_calculator.py
+++ CampusX/Session8 OOP part2/1cordinate_calculator.py
@@ -21,8 +21,34 @@ class point:
         #return ((self.x_cod)**2+(self.y_cod)**2)**0.5
         return self.euclidean_distance(point(0,0))
     
+
+class line:
+    def __init__(self,A,B,C):
+        self.A = A
+        self.B = B
+        self.C = C
+
+    def __str__(self):
+        return '{}x + {}y + {} = 0'.format(self.A, self.B, self.C)
+    
+    def point_on_line(line,point):
+        if line.A*point.x_cod + line.B*point.y_cod + line.C == 0:
+            return "lies on the line"
+        else:
+            return "Not lies on the line"
+
+    def shortest_distance(line,point):
+        return abs(line.A*point.x_cod + line.B*point.y_cod + line.C) / (line.A**2 + line.B**2)**0.5
+    
 p1 = point(0,0)
 p2 = point(1,1)
-
 print(p1.euclidean_distance(p2))
 print(p1.distance_from_origin())
+
+l1 = line(1,1,-2)
+p3 = point(1,1)
+print(l1.point_on_line(p3))
+
+l2 = line(1,1,-2)
+p4 = point(1,5)
+print(l2.shortest_distance(p4))
