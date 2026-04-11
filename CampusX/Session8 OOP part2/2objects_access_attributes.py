4f4d4123a9ca5b664125e30775774ce4d85a3d31
Fahad-78 <fahadsorkar23@gmail.com>
2026-04-11 22:08:09 +0600

    Refactor example usage of person class to demonstrate attribute and method access

--- CampusX/Session8 OOP part2/2objects_access_attributes.py
+++ CampusX/Session8 OOP part2/2objects_access_attributes.py
@@ -13,4 +13,17 @@ class person:
 n1 = input("Enter your name: ")
 c1 = input("Enter your country: ")
 p1 = person(n1,c1)
+
+#How to access atributes
+print(p1.name)
+print(p1.country)
+
+#How to access methods
 p1.greet()
+
+#What if I try to access non-existent attributes
+    #p1.gender
+#output: I will get an error
+
+p1.gender = 'male'
+print(p1.gender)
